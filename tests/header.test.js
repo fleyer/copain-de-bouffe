56d48c08fc2f00864b8eef34cbcaabfa7cf71fcb
Romain <rmanchado@pilot-things.com>
2020-11-04 18:47:57 -0500

    github action
    add failing test

--- tests/header.test.js
+++ tests/header.test.js
@@ -4,7 +4,15 @@ import Header from '../src/components/header';
 import { shallow } from 'enzyme';
 
 describe('Initial Test of the Header', () => {
-	test('Header renders 3 nav items', () => {
+	test('Header renders 1 nav items', () => {
+		const context = shallow(<Header />);
+		expect(context.find('h1').text()).toBe('Copain de bouffe');
+		expect(context.find('Link').length).toBe(1);
+	});
+});
+
+describe('Failing Test of the Header', () => {
+	test('Header renders 2 nav items', () => {
 		const context = shallow(<Header />);
 		expect(context.find('h1').text()).toBe('Copain de bouffe');
 		expect(context.find('Link').length).toBe(1);
